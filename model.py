4bc4c8146b8ad7c4fc6b8b78b4b6cb9242221369
MidhulaJoga <midhula.joga1234@gmail.com>
2026-03-09 15:05:07 +0530

    first commit

--- model.py
+++ model.py
@@ -1,31 +1,54 @@
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-import pickle
 
-df = pd.read_csv("student_data.csv")
 
-X = df[["attendance", "study_hours", "previous_marks"]]
+df=pd.read_csv("student_performance.csv")
+
+num_cols = df.select_dtypes(include=['int64', 'float64']).columns
+for col in num_cols:
+    Q1 = df[col].quantile(0.25) #25th percentile
+    Q3 = df[col].quantile(0.75) #75th percentile
+    IQR = Q3 - Q1 #spread of middle 50% of data is seen
+    lower = Q1 - 1.5 * IQR #standard statistical rule
+    upper = Q3 + 1.5 * IQR
+    df = df[(df[col] >= lower) & (df[col] <= upper)]
+
+def result_score_calc(row):
+    a = 0
+    if (row['total_score'] < 75 ):
+        a=a+1
+    if (row['attendance_percentage'] < 75 ):
+        a=a+1
+    if(row['weekly_self_study_hours'] < 18):
+        a=a+1
+
+    if(a>=2):
+        score=0
+    else:
+        score=1
+    return score
+
+df['result'] = df.apply(result_score_calc, axis=1)
+
+X = df.values
+print(X.shape)
+
+df = df.drop(columns=["student_id", "class_participation","grade"])
+
+X = df.drop("result", axis=1)
 y = df["result"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
 
-# Logistic Regression
-lr_model = LogisticRegression()
-lr_model.fit(X_train, y_train)
-lr_pred = lr_model.predict(X_test)
-lr_accuracy = accuracy_score(y_test, lr_pred)
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
 
-# Random Forest
-rf_model = RandomForestClassifier()
-rf_model.fit(X_train, y_train)
-rf_pred = rf_model.predict(X_test)
-rf_accuracy = accuracy_score(y_test, rf_pred)
+model.fit(X,y)
 
-pickle.dump(lr_model, open("student_model.pkl", "wb"))
-pickle.dump(rf_model, open("rf_model.pkl", "wb"))
+y_pred=model.predict(X_test)
+y_pred
 
-print("Logistic Accuracy:", lr_accuracy)
-print("Random Forest Accuracy:", rf_accuracy)
+df.to_csv("student_10k_dataset.csv", index=False)
+
+print("Dataset generated")
